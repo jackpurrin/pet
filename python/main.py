87522a4a34aa8ba98cd879c82117f1c8b56e642d
~autumn <jackpurrin@tutamail.com>
2025-11-12 15:31:27 -0700

    saving point cuz i wanna take a break.

--- python/main.py
+++ python/main.py
@@ -3,6 +3,7 @@ import sys
 
 sponge = pygame.image.load("assets/images/sponge.png")
 imagerect = sponge.get_rect()
+money = 0
 
 white = (255, 255, 255)
 green = (0, 255, 0)
@@ -36,13 +37,33 @@ while running:
     screen.fill((30, 30, 30))
 
     # This block generates an image
-    screen.blit(sponge, imagerect)
+    class Sprite(pygame.sprite.Sprite):
+        def __init__(self, color, height, width):
+            super().__init__()
+
+            self.image = pygame.Surface([width, height])
+            self.image.fill(SURFACE_COLOR)
+            self.image.set_colorkey(COLOR)
+
+            pygame.draw.rect(self.image,
+                            color,
+                            pygame.Rect(0, 0, width, height))
+
+            self.rect = self.image.get_rect()
+
+    object = Sprite(RED,WIDTH,HEIGHT)
 
     # This is where we make a text box
-    text = font.render('freakbob', True, white, (30,30,30))
+    text = font.render(str(money), True, white, (30,30,30))
     textbox = text.get_rect()
     textbox.center = (WIDTH // 2, HEIGHT // 2)
     screen.blit(text, textbox)
+
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:  #Better to seperate to a new if statement aswell, since there's more buttons that can be clicked and makes for cleaner code.
+            if event.button == 1:
+                money = money =+ 1
+                print(money)
 
     # Update the display
     pygame.display.flip()
