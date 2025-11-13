977730ccde8b98643c7abf5d1dca939fc3b9124b
~autumn <jackpurrin@tutamail.com>
2025-11-12 19:09:32 -0700

    pushes the old code to main.py, starting to experiment with sprites in test.py

--- python/test.py
+++ python/test.py
@@ -1,21 +1,40 @@
 import arcade
 
-WINDOW_WIDTH = 640
-WINDOW_HEIGHT = 480
-WINDOW_TITLE = "Starting Template"
 
 class GameView(arcade.View):
+
     def __init__(self):
         super().__init__()
-        self.background_color = arcade.color.GRAY
+        # 1. Create the SpriteList
+        self.sprites = arcade.SpriteList()
+
+        # 2. Create & append your Sprite instance to the SpriteList
+        self.circle = arcade.Sprite()  # Sprite with the default texture
+        self.circle.position = self.center  # center sprite on screen
+        self.sprites.append(self.circle)  # Append the instance to the SpriteList
+
     def on_draw(self):
+        # 3. Clear the screen
         self.clear()
- 
+
+        # 4. Call draw() on the SpriteList inside an on_draw() method
+        self.sprites.draw()
+
+
 def main():
-    window = arcade.Window(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
+    """ Main function """
+    # Create a window class. This is what actually shows up on screen
+    window = arcade.Window(640, 480, "Minimal SPrite Example (test.py)")
+
+    # Create and setup the GameView
     game = GameView()
+
+    # Show GameView on screen
     window.show_view(game)
+
+    # Start the arcade game loop
     arcade.run()
+
 
 if __name__ == "__main__":
     main()
