d85d9c664be87eca706cd640afcaaa8e84ef1c1a
~autumn <jackpurrin@tutamail.com>
2025-12-03 16:46:34 -0700

    adds a hello world message

--- lua/main.lua
+++ lua/main.lua
@@ -1,6 +1,9 @@
 function love.load()
-    whale = love.graphics.newImage("assets/img/cat.png")
+    love.graphics.setBackgroundColor(0.2,0,0)
 end
 function love.draw()
-    love.graphics.draw(whale, 300, 200)
+    local width, height = love.graphics.getDimensions()
+    width = width / 2
+    height = height / 2
+    love.graphics.print("Hello World!", width, height)
 end
