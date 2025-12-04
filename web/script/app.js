ab9918aff58fb310dba89914c91ca0ab32209993
~autumn <jackpurrin@tutamail.com>
2025-12-03 18:08:13 -0700

    cleans up my adopt page

--- web/script/app.js
+++ web/script/app.js
@@ -1,15 +1,20 @@
-function start() {
-    let type = prompt("Do you want a cat or a dog?")
-    let name = prompt("What is your animals name?")
+var type;
+var gender;
+var name;
 
-    console.log(type)
-    console.log(name)
 
-    if (type == "cat") {
-        document.getElementById("pet").src = "assets/img/cat.jpg";
-    } else if (type == "dog") {
-        document.getElementById("pet").src = "assets/img/dog.png";
-    }
+// function start() {
+//     let type = prompt("Do you want a cat or a dog?")
+//     let name = prompt("What is your animals name?")
 
-    document.getElementById("name").innerHTML = name;
-}
+//     console.log(type)
+//     console.log(name)
+
+//     if (type == "cat") {
+//         document.getElementById("pet").src = "assets/img/cat.jpg";
+//     } else if (type == "dog") {
+//         document.getElementById("pet").src = "assets/img/dog.png";
+//     }
+
+//     document.getElementById("name").innerHTML = name;
+// }
